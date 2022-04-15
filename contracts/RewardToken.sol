d86eb6f3939b52075f6ff19ffb006985ca7f6579
Syed Ali Hamdani <ali.hamdani@bigimmersive.com>
2022-04-15 03:30:05 -0700

    modify the Reward contract

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,11 +1,11 @@
-     // SPDX-License-Identifier: MIT
+      // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardToken is ERC20Capped,Ownable{
     constructor() ERC20Capped(5000000000000*10**decimals()) ERC20("RewardToken","RT"){
-        
+        _mint(msg.sender,500000000);
     }
     function mint(address _to,uint _amount) external onlyOwner(){
         _mint(_to,_amount);
